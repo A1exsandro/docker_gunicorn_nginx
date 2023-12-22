ce176c649bcabec58429fbbe467d299e2e553d13
A1exsandro <alex.con100trado@gmail.com>
2023-12-22 11:49:44 -0400

    add nginx

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Docker Gunicorn e Ngnix
+
+## Some Comands
+* docker-compose up
+* docker-compose up -d --build --scale app=3
+* docker-compose down
+* docker ps
--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello, this is an aplication"
+    return f"Hello, this is an aplication with Container ID: {socket.gethostname()}"
 
 
 if __name__ == "__main__":
